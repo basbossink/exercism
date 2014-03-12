963cf7ace0a4b0d9b747bf8923a50de10c91a371
Bas Bossink <bas.bossink@gmail.com>
2014-03-12 19:02:19 +0100

    Revise after comments from @etrepum.

--- haskell/rna-transcription/DNA.hs
+++ haskell/rna-transcription/DNA.hs
@@ -1,29 +1,16 @@
 {-
-
-Copyright Bas Bossink (bas.bossink@gmail.com)
-This file is part of  DNA.
-
-DNA is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-DNA is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with DNA. If not, see <http://www.gnu.org/licenses/>.
-
+Copyright 2014 Bas Bossink (bas.bossink@gmail.com).
 -}
 
-module DNA where
+module DNA (toRNA) where
 
+toRNA :: String -> String
 toRNA = map toRNAChar
-    where toRNAChar 'C' = 'G' 
-          toRNAChar 'G' = 'C' 
-          toRNAChar 'A' = 'U' 
-          toRNAChar 'T' = 'A' 
-          toRNAChar _ = undefined
+  where 
+  toRNAChar c = case c of 
+    'C' -> 'G' 
+    'G' -> 'C' 
+    'A' -> 'U' 
+    'T' -> 'A' 
+    _ -> undefined
 
