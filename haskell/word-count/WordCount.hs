d92fbaa567352f9838a36db10e9e48a1c57a3f17
Bas Bossink <bas.bossink@gmail.com>
2014-03-14 02:57:37 +0100

    First iteration of word count.

---
+++ haskell/word-count/WordCount.hs
@@ -0,0 +1,29 @@
+{-
+Copyright 2014 Bas Bossink (bas.bossink@gmail.com).
+-}
+module WordCount (wordCount) where
+
+import Data.Char (isLetter, toLower, isSpace, isPunctuation, isSymbol)
+import Data.Map (Map, empty, adjust, insert, member)
+
+split phrase = 
+  case dropWhile toBeIgnored phrase of
+    "" -> []
+    letters -> letter : split splitted
+      where 
+      (letter, splitted) = break toBeIgnored letters
+  where
+  toBeIgnored = \char -> or [isSpace char, isPunctuation char, isSymbol char]
+
+tallyWords words = tallyWords' words empty
+  where 
+  tallyWords' (word:restOfWords) m = case member loweredWord m of
+    True -> tallyWords' restOfWords $ adjust (1 +) loweredWord m 
+    False -> tallyWords' restOfWords $ insert loweredWord 1 m 
+    where
+      loweredWord = map toLower word
+  tallyWords' [] m = m
+    
+wordCount :: String -> Map String Integer
+wordCount = tallyWords . split
+
