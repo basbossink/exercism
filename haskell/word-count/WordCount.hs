3b4d39cc7ded9de6ebd1c168e246569ea3dc1e49
Bas Bossink <bas.bossink@gmail.com>
2014-03-19 22:35:33 +0100

    Update version using fold', to use a separate normalize function.

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -5,14 +5,17 @@ module WordCount (wordCount) where
 
 import Data.Char (isAlphaNum, toLower)
 import Data.Foldable (foldl')
-import Data.List.Split (wordsBy)
-import Data.Map (Map, empty, insertWith)
-
-tallyWords :: [String] -> Map String Integer
-tallyWords words' = foldl' tallyWord empty words' 
-  where 
-  lowerWord = map toLower 
-  tallyWord m word = insertWith (+) (lowerWord word) 1 m
+import Data.Map.Strict (Map, empty, insertWith)
 
 wordCount :: String -> Map String Integer
-wordCount = tallyWords . wordsBy (not . isAlphaNum)
+wordCount = tallyWords . words . map normalize
+
+tallyWords :: [String] -> Map String Integer
+tallyWords = foldl' tallyWord empty 
+  where 
+  tallyWord accumulator word = insertWith (+) word 1 accumulator
+
+normalize :: Char -> Char
+normalize c 
+  | isAlphaNum c = toLower c
+  | otherwise = ' '
