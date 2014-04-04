d663df874c4bf2d71f8ecee00d0cc09ce40c5940
Bas Bossink <bas.bossink@gmail.com>
2014-04-04 23:56:58 +0200

    Replace split with string.Fields.
    Replace regexp replace with simple rune substitution.

--- go/word-count/histogram.go
+++ go/word-count/histogram.go
@@ -1,7 +1,6 @@
 package wc
 
 import (
-	"regexp"
 	"strings"
 )
 
@@ -10,22 +9,17 @@ type Histogram map[string]int
 func WordCount(text string) Histogram {
 	normalized := normalize(text)
 	result := Histogram{}
-	for _, word := range split(normalized) {
+	for _, word := range strings.Fields(normalized) {
 		result[word]++
 	}
 	return result
 }
 
 func normalize(text string) string {
-	nonAlphaNumeric := regexp.MustCompile(`[^a-zA-Z0-9]`)
-	return strings.ToLower(nonAlphaNumeric.ReplaceAllString(text, " "))
-}
-
-func split(text string) []string {
-	result := []string{}
-	words := strings.Fields(text)
-	for _, word := range words {
-		result = append(result, word)
-	}
-	return result
+	return strings.Map(func(r rune) rune {
+		if r >= 'a' && r <= 'z' || r >= '0' && r <= '9' {
+			return r
+		}
+		return ' '
+	}, strings.ToLower(text))
 }
