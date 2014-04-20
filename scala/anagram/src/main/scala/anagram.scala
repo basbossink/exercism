215d49be61e85000075da75b60da005bd759f880
Bas Bossink <bas.bossink@gmail.com>
2014-04-20 20:18:28 +0200

    Finish anagram.

--- scala/anagram/src/main/scala/anagram.scala
+++ scala/anagram/src/main/scala/anagram.scala
@@ -1,5 +1,23 @@
+import scala.collection.mutable.Map
+
 class Anagram(word: String) {
+  def characterHistogram(word: String) = {
+    word.foldLeft(Map[Char,Int]())( 
+      (accumulator, char) => {
+        val loweredChar = char.toLower
+        val oldValue = 
+          if (accumulator.contains(loweredChar)) 
+            accumulator(loweredChar) 
+          else 
+            0
+        accumulator += (loweredChar -> (oldValue + 1))
+      })
+  }
+  val histogram = characterHistogram(word)
   def matches(candidates: Seq[String]): Seq[String] = {
-    Seq()
+    candidates.filter(
+      candidate => candidate.toLowerCase != word.toLowerCase && 
+        candidate.length == word.length &&
+        histogram == characterHistogram(candidate))
   }
 }
