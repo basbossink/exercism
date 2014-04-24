0860a259cade9618a3091718d0810468e3a4756c
Bas Bossink <bas.bossink@gmail.com>
2014-04-24 22:50:58 +0200

    Reformat nucleotide_count.

--- scala/nucleotide-count/src/main/scala/nucleotide_count.scala
+++ scala/nucleotide-count/src/main/scala/nucleotide_count.scala
@@ -1,31 +1,32 @@
-import scala.collection.mutable.Map
+import scala.collection.mutable
 
 class DNA(string: String) {
   val dnaNucleotides = List('A', 'G', 'C', 'T')
-  def validDnaNucleotide(string: String) = string.forall(dnaNucleotides.contains(_))
-  require(validDnaNucleotide(string))
-  
-  def characterHistogram(word: String) = {
-    word.foldLeft(Map[Char,Int]())( 
-      (accumulator, char) => {
-        val oldValue = withDefaultZero(accumulator, char)
-        accumulator += (char -> (oldValue + 1))
-      })
-  }
 
-  def withDefaultZero(hist: Map[Char,Int],key: Char):Int =
-    if(hist.contains(key)) hist(key) else 0
+  def validDnaNucleotide(string: String) = string forall (dnaNucleotides.contains(_))
+
+  require(validDnaNucleotide(string))
+
+  def characterHistogram(word: String) = (word foldLeft mutable.Map[Char, Int]())(
+    (accumulator, char) => {
+      val oldValue = withDefaultZero(accumulator, char)
+      accumulator += (char -> (oldValue + 1))
+    })
+
+  def withDefaultZero(hist: mutable.Map[Char, Int], key: Char) = hist.get(key) match {
+    case Some(v) => v
+    case None => 0
+  }
 
   val histogram = characterHistogram(string)
 
-  def count(nucleotide: Char):Int = {
-    require(validDnaNucleotide(nucleotide.toString) ||
-      nucleotide == 'U')
+  def count(nucleotide: Char) = {
+    require(validDnaNucleotide(nucleotide.toString) || nucleotide == 'U')
     withDefaultZero(histogram, nucleotide)
   }
 
-  def nucleotideCounts():Map[Char,Int] = 
-    Map('A' -> withDefaultZero(histogram, 'A'), 
+  def nucleotideCounts() =
+    mutable.Map('A' -> withDefaultZero(histogram, 'A'),
       'C' -> withDefaultZero(histogram, 'C'),
       'G' -> withDefaultZero(histogram, 'G'),
       'T' -> withDefaultZero(histogram, 'T'))
