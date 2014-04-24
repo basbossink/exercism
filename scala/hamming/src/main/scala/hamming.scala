f36daf20c96e0d1beaed9b6025e4a7a5f3b11279
Bas Bossink <bas.bossink@gmail.com>
2014-04-24 22:25:41 +0200

    Use infix notation for zip.

--- scala/hamming/src/main/scala/hamming.scala
+++ scala/hamming/src/main/scala/hamming.scala
@@ -1,8 +1,6 @@
 object Hamming {
   def compute(first: String, second: String) = { 
-    first.zip(second).
-      filter(pair => pair._1 != pair._2).
-      length
+    (first zip second).filter(p => p._1 != p._2).length
   }
 }
  
