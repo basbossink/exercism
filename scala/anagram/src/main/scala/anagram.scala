9a8774e092e8f1c9415e3826b34fdc074ec01889
Bas Bossink <bas.bossink@gmail.com>
2014-04-24 22:13:29 +0200

    Change anagram implementation to use sorted.

--- scala/anagram/src/main/scala/anagram.scala
+++ scala/anagram/src/main/scala/anagram.scala
@@ -1,23 +1,10 @@
-import scala.collection.mutable.Map
-
 class Anagram(word: String) {
-  def characterHistogram(word: String) = {
-    word.foldLeft(Map[Char,Int]())( 
-      (accumulator, char) => {
-        val loweredChar = char.toLower
-        val oldValue = 
-          if (accumulator.contains(loweredChar)) 
-            accumulator(loweredChar) 
-          else 
-            0
-        accumulator += (loweredChar -> (oldValue + 1))
-      })
-  }
-  val histogram = characterHistogram(word)
-  def matches(candidates: Seq[String]): Seq[String] = {
+  val loweredWord = word.toLowerCase
+
+  def matches(candidates: Seq[String]): Seq[String] =
     candidates.filter(
-      candidate => candidate.toLowerCase != word.toLowerCase && 
-        candidate.length == word.length &&
-        histogram == characterHistogram(candidate))
-  }
+      candidate => {
+        val loweredCandidate = candidate.toLowerCase
+        loweredWord != loweredCandidate && loweredWord.sorted == loweredCandidate.sorted
+      })
 }
