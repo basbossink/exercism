d6f8746f3449e92f7347a99fd6f5ac2bc4349eb8
Bas Bossink <bas.bossink@gmail.com>
2014-05-07 00:38:21 +0200

    Use zip instead of explicit creation of the map.

--- scala/nucleotide-count/build.sbt
+++ scala/nucleotide-count/build.sbt
@@ -2,4 +2,3 @@ scalaVersion := "2.10.3"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
 
-seq(ScctPlugin.instrumentSettings : _*)
---
+++ scala/nucleotide-count/src/main/scala/DNA.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable
+
+class DNA(nucleotides: String) {
+  val dnaNucleotides = List('A', 'G', 'C', 'T')
+
+  def validDnaNucleotide(word: String) = word forall (dnaNucleotides.contains(_))
+
+  require(validDnaNucleotide(nucleotides))
+
+  val nucleotideCounts = mutable.Map[Char, Int]() ++
+    (dnaNucleotides zip (dnaNucleotides map (c => nucleotides count (_ == c))))
+
+  def count(nucleotide: Char) = {
+    require(validDnaNucleotide(nucleotide.toString) || nucleotide == 'U')
+    nucleotideCounts.getOrElse(nucleotide, 0)
+  }
+}
