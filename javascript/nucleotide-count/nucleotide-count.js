5c8d28633fb09f3d577e6fac98100b26e00c972d
Bas Bossink <bas.bossink@gmail.com>
2015-06-04 02:16:11 +0200

    Reformat and inline nucleotides.

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -7,18 +7,20 @@
   'use strict';
   var dna = function (strand) {
     strand = strand || '';
-    var nucleotides = [ 'A', 'C', 'G', 'T' ],
-      count = function (nucleotide) {
-        return strand.split('').reduce(function (prev, curr) {
-          return prev + (curr === nucleotide ? 1 : 0);
-        }, 0);
-      },
+
+    var count = function (nucleotide) {
+      return strand.split('').reduce(function (prev, curr) {
+        return prev + (curr === nucleotide ? 1 : 0);
+      }, 0);
+    },
+
       histogram = function () {
-        return nucleotides.reduce(function (prev, curr) {
+        return [ 'A', 'C', 'G', 'T' ].reduce(function (prev, curr) {
           prev[curr] = count(curr);
           return prev;
         }, {});
       };
+
     if (!/^[ACGT]*$/.test(strand)) {
       throw new Error("Invalid strand");
     }
