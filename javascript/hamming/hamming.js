2e01f4fc507bf55d24a4b2fa3fd55c6d5e50d4f3
Bas Bossink <bas.bossink@gmail.com>
2015-06-04 02:08:56 +0200

    Enable all test cases, use reduce instead of for loop.

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -5,17 +5,12 @@
 (function () {
   'use strict';
   var compute = function (first, second) {
-    var firstLength = first.length, secondLength = second.length, result = 0,
-      i;
-    if (firstLength !== secondLength) {
+    if (first.length !== second.length) {
       throw new Error('DNA strands must be of equal length.');
     }
-    for (i = 0; i < firstLength; i += 1) {
-      if (first[i] !== second[i]) {
-        result += 1;
-      }
-    }
-    return result;
+    return first.split('').reduce(function (prev, curr, i) {
+      return prev + (curr !== second.charAt(i) ? 1 : 0);
+    }, 0);
   };
   module.exports.compute = compute;
 }());
